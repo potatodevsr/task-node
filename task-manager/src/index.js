aa774e7f3c424786ec283ea3e70414de02dba157
potatodevsr <hoshiku@gmail.com>
2023-10-01 15:49:04 +0700

    Resource Updating Endpoints: Part II

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -97,6 +97,30 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send()
     }
 })
+app.patch('/tasks/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['completed', 'description']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    console.log('isValidOperation = ', isValidOperation);
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+
+
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
