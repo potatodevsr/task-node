5845f209ec158b154a0949a44ba69b16b79993b1
potatodevsr <hoshiku@gmail.com>
2023-10-01 13:27:12 +0700

    Async/Await: Part II

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,14 +1,26 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findOneAndDelete('6517a220a84ecd312068455')
-    .then((task) => {
-        console.log('Delete task by id:', task);
-        return Task.countDocuments({ completed: false });
-    })
-    .then((result) => {
-        console.log('Result:', result);
-    })
-    .catch((e) => {
-        console.log('Error:', e);
-    });
+// Task.findOneAndDelete('6517c46a73ac8967e1b594da')
+//     .then((task) => {
+//         console.log('Delete task by id:', task);
+//         return Task.countDocuments({ completed: false });
+//     })
+//     .then((result) => {
+//         console.log('Result:', result);
+//     })
+//     .catch((e) => {
+//         console.log('Error:', e);
+//     });
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndUpdate(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('6517c46a73ac8967e1b594da').then((count) => {
+    console.log('count', count);
+}).catch((e) => {
+    console.log('Error deleteTaskAndCount = ', e);
+})
