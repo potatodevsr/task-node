768306f5ac212ddf8f6101ed1502299d2b076784
potatodevsr <hoshiku1997@gmail.com>
2023-10-18 09:38:30 +0700

    UPD Exploring SendGrid

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,11 +1,44 @@
-const mailjetMail = require('@mailjet/mail')
-const mailjetAPIKey = '70d3d45e7c4ce72d584afb24cc16dafd'
+/**
+ * @typedef {import('node-mailjet')} 
+ */
 
-mailjetMail.setApiKey(mailjetAPIKey)
+const MJ_ID = '70d3d45e7c4ce72d584afb24cc16dafd'
+const MJ_TOKEN = '9c499d4659d973ef28474ea87bba490c'
 
-mailjetMail.send({
-    to: 'chada@srkn.io',
-    from: 'chada@srkn.io',
-    subject: 'This is my first creation!',
-    text: 'Hello, this is the text content of the email.'
-})
+
+const Mailjet = require('node-mailjet');
+const mailjet = Mailjet.apiConnect(
+    MJ_ID,
+    MJ_TOKEN,
+);
+
+
+const request = mailjet
+    .post('send', { version: 'v3.1' })
+    .request({
+        Messages: [
+            {
+                From: {
+                    Email: "hoshiku1997@gmail.com",
+                    Name: "Mailjet Pilot"
+                },
+                To: [
+                    {
+                        Email: "hoshiku1997@gmail.com",
+                        Name: "passenger 1"
+                    }
+                ],
+                Subject: "Your email flight plan!",
+                TextPart: "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
+                HTMLPart: "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!"
+            }
+        ]
+    })
+
+request
+    .then((result) => {
+        console.log(result.body)
+    })
+    .catch((err) => {
+        console.log(err.statusCode)
+    })
