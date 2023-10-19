02b303dfdfa5af465b85494b9903d881ace8fd46
potatodevsr <hoshiku1997@gmail.com>
2023-10-19 13:51:32 +0700

    Testing with Authentication

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,17 +1,24 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'Chaya',
     email: 'chaya@example.com',
-    password: 'MyPass888!'
+    password: 'MyPass888!',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
 }
+
 beforeEach(async () => {
     await User.deleteMany()
     await new User(userOne).save()
 })
-
 
 test('hould signup a new user', async () => {
     await request(app).post('/users').send({
@@ -33,4 +40,34 @@ test('Should login nonexisting user', async () => {
         email: userOne.email,
         password: 'thisisnotmypass'
     }).expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('Should not delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401) // Expect a 401 status code for unauthenticated user
 })
