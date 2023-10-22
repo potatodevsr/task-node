a801c3e48f2b4068c4d8e7d9d98749827e7c70b2
potatodevsr <hoshiku1997@gmail.com>
2023-10-22 10:15:32 +0700

    Event Acknowledgements

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -3,17 +3,23 @@ const socket = io()
 socket.on('message', (message) => {
     console.log(message)
 })
+
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message deliverd!')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
     // navigator.geolocation
-
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
@@ -22,6 +28,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!');
         })
     })
 })
